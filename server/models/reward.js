756bd0585ed25b5c258b10cabf4c6f5b58fae5f9
Dan Steshenko <dan.steshenko@gmail.com>
2017-12-19 23:32:38 +0300

    Code style in models

--- server/models/reward.js
+++ server/models/reward.js
@@ -2,11 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RewardSchema = new Schema({
-    id: {type: Number, required: true},
-    price: {type: Number, required: true},
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    picture: {type: String, required: true}
+  id: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  picture: {
+    type: String,
+    required: true
+  }
 })
 
 module.exports = mongoose.model('Reward', RewardSchema);
