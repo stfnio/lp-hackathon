2ff14c546c3cf051bed545e824c3d21df6f5ac53
Dan Steshenko <dan.steshenko@gmail.com>
2017-12-22 00:28:07 +0300

    Done client authorization

--- react-ui/src/actions/index.js
+++ react-ui/src/actions/index.js
@@ -1,31 +1,25 @@
 import axios from 'axios';
-import {
-  AUTH_USER
-} from './types';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:5000';
 
-export function logInUser({
-    tokenId
-  },
-  redirectToHomePage
-) {
+export function logInUser({ tokenId }, redirectToHomePage) {
   return dispatch => {
-    axios.post({
-      method: 'post',
-      url: `${ROOT_URL}/auth`,
-      headers: {
-        'Authorization': tokenId
-      }
-    }).then((user) => {
-      dispatch({
-        type: AUTH_USER
+    axios({
+        method: 'post',
+        url: `${ROOT_URL}/auth`,
+        headers: {
+          Authorization: tokenId
+        }
       })
+      .then(user => {
+        dispatch({
+          type: AUTH_USER
+        });
 
-      localStorage.setItem('token', tokenId);
-      localStorage.setItem('user', user);
+        localStorage.setItem('token', tokenId);
 
-      redirectToHomePage();
-    })
-  }
+        redirectToHomePage();
+      });
+  };
 }
--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import jwt_decode from 'jwt-decode'
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App';
@@ -12,12 +13,21 @@ import Home from './containers/Home';
 import LogIn from './containers/LogIn';
 import PrivateRoute from './containers/PrivateRoute';
 
-import reducers from './reducers/index'
+import reducers from './reducers/index';
+import { AUTH_USER } from './actions/types';
 
-const store = createStore(
-  reducers,
-  applyMiddleware(reduxThunk)
-)
+const store = createStore(reducers, applyMiddleware(reduxThunk));
+
+const token = localStorage.getItem('token');
+// update application state with token information if needed
+if (token) {
+  // update authentication flag
+  const decoded_token = jwt_decode(token);
+
+  console.log(decoded_token)
+
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
   <Provider store={store}>
@@ -29,7 +39,8 @@ ReactDOM.render(
         </Switch>
       </App>
     </Router>
-  </Provider>
-  , document.getElementById('root'));
+  </Provider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
