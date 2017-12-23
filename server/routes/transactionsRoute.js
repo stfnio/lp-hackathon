468fee6f8ba4658daa338a051eeb7f8b39980b41
steve <jam.prosteve@gmail.com>
2017-12-23 22:16:14 +0300

    transaction creation via validator request - done

--- server/routes/transactionsRoute.js
+++ server/routes/transactionsRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const TransactionModel = require('../models/transaction');
 const UserModel = require('../models/user');
+const RewardModel = require('../models/reward');
 
 router.post('/', (req, res) => {
   TransactionModel.find()
@@ -14,10 +15,30 @@ router.post('/', (req, res) => {
 });
 
 router.post('/fromValidator', (req, res) => {
-  UserModel.findOne({ _id: req.body.user })
-    .exec()
-    .then(user => {
-      res.sendStatus(200);
+  Promise.all([
+    UserModel.findOne({ _id: req.body.user }),
+    RewardModel.findOne({ _id: req.body.reward })
+  ])
+    .then(([user, reward]) => {
+      console.log(reward.price);
+      if (user.balance >= reward.price) {
+        const tr = new TransactionModel({
+          user: req.body.user,
+          operationType: 'Debit',
+          amount: reward.price,
+          reward: req.body.reward
+        });
+        tr.save(err => {
+          if (err) throw err;
+        });
+        user.balance = user.balance - reward.price;
+        user.save(err => {
+          if (err) throw err;
+        });
+        res.status(200).json({ message: 'Выдайте подарок ' + reward.title });
+      } else {
+        res.status(400).json({ error: 1 });
+      }
     })
     .catch(err => {
       throw err;
