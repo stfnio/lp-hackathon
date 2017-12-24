c9fe18baaa675c8de971d2b8ef256195018210da
steve <jam.prosteve@gmail.com>
2017-12-24 15:20:36 +0300

    balance accrual & debit was implemented

--- server/routes/transactionsRoute.js
+++ server/routes/transactionsRoute.js
@@ -5,7 +5,18 @@ const UserModel = require('../models/user');
 const RewardModel = require('../models/reward');
 
 router.post('/', (req, res) => {
-  
+  UserModel.findOne({ _id: req.body.user })
+    .then(user => {
+      const success = handleBalanceOperation(user, req.body.operationType, req.body.amount);
+      if (success) {
+        res.sendStatus(201);
+      } else {
+        res.status(400).json({ error: 1})
+      }
+    })
+    .catch(err => {
+      throw err;
+    });
 });
 
 router.post('/fromValidator', (req, res) => {
@@ -14,20 +25,13 @@ router.post('/fromValidator', (req, res) => {
     RewardModel.findOne({ _id: req.body.reward })
   ])
     .then(([user, reward]) => {
-      if (user.balance >= reward.price) {
-        const tr = new TransactionModel({
-          user: req.body.user,
-          operationType: 'Debit',
-          amount: reward.price,
-          reward: req.body.reward
-        });
-        tr.save(err => {
-          if (err) throw err;
-        });
-        user.balance = user.balance - reward.price;
-        user.save(err => {
-          if (err) throw err;
-        });
+      const success = handleBalanceOperation(
+        user,
+        'Debit',
+        reward.price,
+        reward
+      );
+      if (success) {
         res.status(201).json({ message: 'Выдайте подарок ' + reward.title });
       } else {
         res.status(400).json({ error: 1 });
@@ -37,5 +41,43 @@ router.post('/fromValidator', (req, res) => {
       throw err;
     });
 });
+
+function handleBalanceOperation(user, type, amount, reward) {
+  switch (type) {
+    case 'Accrual':
+      const tr = new TransactionModel({
+        user: user._id,
+        operationType: 'Accrual',
+        amount: amount
+      });
+      tr.save(err => {
+        if (err) throw err;
+      });
+      user.balance = user.balance + amount;
+      user.save(err => {
+        if (err) throw err;
+      });
+      return true;
+    case 'Debit':
+      if (user.balance >= amount) {
+        const tr = new TransactionModel({
+          user: user._id,
+          operationType: 'Debit',
+          amount: amount,
+          reward: reward ? reward._id : null
+        });
+        tr.save(err => {
+          if (err) throw err;
+        });
+        user.balance = user.balance - amount;
+        user.save(err => {
+          if (err) throw err;
+        });
+        return true;
+      } else {
+        return false;
+      }
+  }
+}
 
 module.exports = router;
