f1a4126fc248980661a6587111e9f57602241eae
steve <jam.prosteve@gmail.com>
2017-12-25 15:40:44 +0300

    added single user route

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const adminRequired = require('../middleware/adminRequired');
 const UserModel = require('../models/user');
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
+  UserModel.find()
+    .populate('group')
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+router.get('/', adminRequired, (req, res) => {
   UserModel.find()
     .populate('group')
     .then(users => {
