2b80171667f3d20110f4323486f93da68389f24c
Dan Steshenko <dan.steshenko@gmail.com>
2017-12-26 14:50:11 +0300

    delete logs

--- react-ui/src/actions/index.js
+++ react-ui/src/actions/index.js
@@ -119,7 +119,6 @@ export function fetchGame() {
         Authorization: localStorage.getItem('token')
       }
     }).then(({ data }) => {
-      console.log('game data', data)
       if (data.game) {
         dispatch({
           type: FETCH_GAME,
